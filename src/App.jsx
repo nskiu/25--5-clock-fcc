316a2627cf7c922723597daed498128bd1248151
nskiu <nyelsku@gmail.com>
2023-06-15 03:56:58 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Controls from "./components/Controls";
+
+const App = () => {
+  return (
+    <div>
+      <Controls />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/Controls.jsx
@@ -0,0 +1,11 @@
+const Controls = () => {
+  return (
+    <>
+      <div>25 + 5 Clock</div>
+      <div>Break Length</div>
+      <div>Session Length</div>
+    </>
+  );
+};
+
+export default Controls;
